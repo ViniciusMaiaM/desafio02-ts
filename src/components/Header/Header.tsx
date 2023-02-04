41fb91cc63b012cffbfb710143a0f29baf4d1400
Vinicius <viniciusmaiamarinho1@gmail.com>
2023-02-04 18:49:15 -0300

    Update Header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,9 +1,14 @@
-import './Header.css'
-
+import { Heading,  Flex, Box } from '@chakra-ui/react'
+import { Logo } from '../svg/Logo'
 export const Header  = () => {
-  return(
-    <div className='header'>
-      Dio Bank
-    </div>
-  )
+	return(
+		<Flex maxWidth='100%' justify='center' bg='#820AD1' paddingTop='8' gap='5'>
+			<Heading size='lg'  fontSize='40px' color='white' fontFamily='Georgia'>
+			Dio Bank
+			</Heading>
+			<Box marginTop='1'>
+				<Logo/>
+			</Box>
+		</Flex>
+	)
 }
