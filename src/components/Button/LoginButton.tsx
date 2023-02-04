899e7b19144f3146d3a48291ca826e6dc947012c
Vinicius <viniciusmaiamarinho1@gmail.com>
2023-02-04 19:23:22 -0300

    Add card props

--- src/components/Button/LoginButton.tsx
+++ src/components/Button/LoginButton.tsx
@@ -1,10 +1,15 @@
 import { Button } from "@chakra-ui/react"
 import { login } from "../../services/login"
 
-export const LoginButton = () => {
+interface ILoginButton {
+    alert: string,
+    action: () => {} | void
+}
+
+export const LoginButton = ({alert, action}: ILoginButton) => {
     return(
-        <Button  onClick={login} colorScheme='purple' size='lg' width='200px' height='58px' marginTop='5px' variant='outline' _hover={{background:'#820AD1', color:'white' }}>
+        <Button  onClick={action} colorScheme='purple' fontWeight='bold' size='lg' width='200px' height='58px' marginTop='5px' variant='outline' _hover={{background:'#820AD1', color:'white' }}>
             Continuar
         </Button>
-    )
+    );
 }
--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,9 +1,11 @@
 import { Header } from "./Header/Header"
-
+import { LoginButton } from "./Button/LoginButton"
+import { login } from "../services/login"
 export const Card = () => {
   return(
     <div>
       <Header />
+      <LoginButton action={login} alert="Login"/>
     </div>
   )
 }
